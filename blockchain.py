587c65370c42e65883d6f7c5229165af5b8fef36
meyson3 <ilijajovic2001@gmail.com>
2025-02-26 16:56:30 +0100

    Added some transaction lines and a generation of a random name for our node.

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,11 @@
 import hashlib
 import json
+from textwrap import dedent
 from time import time
 from uuid import uudi4
+
+from flask import Flask
+
 
 class Blockchain(object):
     def__init__(self):
@@ -85,5 +89,32 @@ class Blockchain(object):
     def last_block(self):
         return self.chain[-1]
         pass
-        
+    
+    #Instantiate our Node
+    app = Flask(__name__)
+    
+    #generate a globally unique address for this node
+    node_identifier = str(uudi4()).replace('-'.'')
+    
+    #Instantiate the Blockchain
+    blockchain = Blockchain()
+    
+    @app.route('/mine', methods=['GET'])
+    def mine():
+        return "We'll mine a new block"
+    
+    @app.route('/transactions/new', methods=['POST'])
+    def new_transaction():
+        return "We'll add a new transaction"
+
+    @app.route('/chain', methods=['GET'])
+    def full_chain():
+        response = {
+            'chain': blockchain.chain,
+            'length': len(blockchain.chain),
+        }
+        return jsonify(response), 200
+
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000)
     
